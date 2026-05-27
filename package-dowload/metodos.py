66779396579d949271fb2054d3585fe0fc56365a
Bruno Bonato <brunobonatosc@hotmail.com>
2026-05-27 12:11:34 -0300

    Mais eventos por mês e Funções de Verificação
    . Possiblidade de criar evento para outros meses
    . Funções de Verificação de data
    . Adequações para as novas funcionalidades

--- package-dowload/metodos.py
+++ package-dowload/metodos.py
@@ -3,7 +3,26 @@ from datetime import date
 import json
 import os
 
+#Funções de verificação
+
+
+def mes_extenso(mes):
+    meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" ]
+    return meses[mes-1]
+
+def verificar_dia(dia,agenda):
+    if not dia.isdigit() or int(dia) <= 0 or int(dia) > 31: #Confirindo se o algarismo digitado realmente é um número válido
+        print("\n[SISTEMA] Por favor, digite uma data válida.\n")
+        menu(agenda)
+
+def verificar_mes(mes,agenda):
+    if mes < 1 or mes > 12: #Confere se o alggorismo digitado realmente é um número válido
+        print("\n[SISTEMA] Por favor, digite uma data válida.\n")
+        menu(agenda)
+
+
 #Funções de Menu e Organização
+
 def menu(agenda):
     data = date.today() #Reconhece a data atual
     ano = data.year
@@ -34,11 +53,14 @@ def menu(agenda):
             agenda = listar(agenda)
 
 def ordenar(agenda):
-    temp = dict(sorted(agenda.items())) #Usa a função 'items' para criar uma lista de pares chave-valor, a função 'sorted' ordena usando Timsort e a função 'dict' transforma em dicionário novamente
+    temp = {}
+    for mes in agenda:
+        ordena_mes = dict(sorted(agenda[mes].items())) #Usa a função 'items' para criar uma lista de pares chave-valor, a função 'sorted' ordena usando Timsort e a função 'dict' transforma em dicionário novamente
+        temp[mes] = ordena_mes
     return temp
 
-#Funções de Permanência de Dados
 
+#Funções de Permanência de Dados
 
 def salvar(agenda):
     with open("dados.json", "w", encoding="utf-8") as arquivo: #Cria e salva os dados no arquivo JSON
@@ -50,27 +72,39 @@ def carregar_dados():
         with open("dados.json", "r") as arquivo:
             return json.load(arquivo)
     else:
-        return {}  
+        return { #Caso a 'agenda' não exista retornamos seu esqueleto 
+            "janeiro": {},
+            "fevereiro": {},
+            "março": {},
+            "abril": {},
+            "maio": {},
+            "junho": {},
+            "julho": {},
+            "agosto": {},
+            "setembro": {},
+            "outubro": {},
+            "novembro": {},
+            "dezembro": {}
+        }  
 
 
 #Funções do CRUD (Adicionar, Remover e Listar)
 
     
 def adicionar_evento(agenda):
-    dia = str(input("Quando será esse evento? *Em numeral* "))   
-    if not dia.isdigit(): #Confirindo se o algarismo digitado realmente é um número válido
-        print("\n[SISTEMA] Por favor, digite uma data válida.\n")
-    elif int(dia) <= 0 or int(dia) > 31:
-        print("\n[SISTEMA] Dia Inexistente.\n")
-        adicionar_evento(agenda)
+    dia = str(input("Quando será esse evento? *Em numeral* "))
+    verificar_dia(dia,agenda)
 
+    mes = int(input("Qual será o mês? *Em numeral* "))
+    verificar_mes(mes,agenda)
+    mes = mes_extenso(mes) #Transforma mês por extenso
 
     titulo = str(input("Digite o título do evento: ")).capitalize()
     comeco = str(input("Que horas começa? 00:00 "))
     final = str(input("Que horas termina? 00:00 "))
     descricao = str(input("Descição do Evento: ")).capitalize()
 
-    agenda[dia] = [titulo, comeco, final, descricao]
+    agenda[mes][dia] = [titulo, comeco, final, descricao]
     print("\nEvento Adicionado\n")
 
 def remover(agenda):
@@ -78,11 +112,16 @@ def remover(agenda):
         print("\nA agenda esta vazia\n")
     
     dia = input("Qual o dia do vento que você deseja remover? *Em numeral* ")
-    if dia in agenda:
-        agenda.pop(dia)
+    verificar_dia(dia,agenda)
+    mes = int(input("Qual o mês do evento? *em numeral* "))
+    verificar_mes(mes,agenda)
+    mes = mes_extenso(mes) #Transforma mês por extenso
+
+    if dia in agenda[mes]:
+        agenda[mes].pop(dia)
         print("Evento deletado.")
     else:
-        print("\nA agenda esta vazia\n")
+        print("\n[SISTEMA] Não há evento nesta data\n")
 
 def listar(agenda):
     agenda = ordenar(agenda)
@@ -92,10 +131,9 @@ def listar(agenda):
     escolha = int(input("Selecione uma opção:  "))
 
     if escolha == 1:
-        for dia in agenda: #Verificando se à agenda não esta vazia
-            if agenda[dia] is not None: #Verificando se o dia apresenta evento
-                print(f"\n{agenda[dia][0]}:\nDia {dia} // {agenda[dia][1]} - {agenda[dia][2]}\n{agenda[dia][3]}\n")
-            else:
-                print(f"\nNão a eventos para o dia {dia}\n")
-                break
-    return agenda
+        for mes in agenda: 
+            if agenda[mes] is not None: #Verificando se o mês não esta vazio
+                for dia in agenda[mes]: 
+                    if dia is not None: #Verifica se o dia não esta vazio
+                        print(f"\n{agenda[mes][dia][0]}:\nDia {dia} de {mes} // {agenda[mes][dia][1]} - {agenda[mes][dia][2]}\nDescrição: {agenda[mes][dia][3]}\n")
+    return agenda #Devolde a agenda ordena para a main
